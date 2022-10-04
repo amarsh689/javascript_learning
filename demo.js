199f323dcf7a3c6130ed2aa1a8c88fff01fbde60
amarsh689 <113324180+amarsh689@users.noreply.github.com>
2022-10-04 14:44:34 +0530

    first program execution
    first program execution

--- demo.js
+++ demo.js
@@ -3,4 +3,3 @@ console.log("Hey there, Im Using Javascript");
 console.info("This  Is Info")// info means information;
 console.warn("im good guy")// warn means warning
 console.error("there should not be error")
-console.trace("trace function is to tracing the")
---
+++ intro.js
@@ -0,0 +1,6 @@
+console.log("Amar Dnyaneshwar Patil")
+console.log("Hobbies- i love to explore about space, Love to play snooker, Love to go and travel alone")
+console.log("Strength- I can adust in any situation and any surrounding, continuous lerner, I always focused towards work or anything what im doing")
+console.log("Weaknesses- I hesitate to ask help, I'm bit introvert type of person, sometimes i get anger quickly because i want everything to be better.")
+console.log("30+40 = 80")
+console.log("49*479 = 23471")
