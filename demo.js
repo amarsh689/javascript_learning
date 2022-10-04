042df016cef938f86499ef56b5e24c15070b3f25
amarsh689 <113324180+amarsh689@users.noreply.github.com>
2022-10-04 08:36:52 +0530

    n
    n

---
+++ demo.js
@@ -0,0 +1,6 @@
+console.log("Hello");
+console.log("Hey there, Im Using Javascript");
+console.info("This  Is Info")// info means information;
+console.warn("im good guy")// warn means warning
+console.error("there should not be error")
+console.trace("trace function is to tracing the")
